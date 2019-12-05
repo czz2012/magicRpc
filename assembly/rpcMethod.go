0cabf6118ea9361ac2c263fd6692144cc4bf4779
yamakiller <296270991@qq.com>
2019-12-05 21:57:27 +0800

    .

---
+++ assembly/rpcMethod.go
@@ -0,0 +1,12 @@
+package assembly
+
+//type RPCMethod func(param proto.Message) proto.Message
+
+/*type RPCHandle struct {
+	_method interface{}
+}*/
+
+/*func (slf *RPCHandle) Test() {
+	f := reflect.ValueOf(slf._method)
+	f.Type().NumOut
+}*/
