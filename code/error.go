50966168f4345ccd76adc3aec104f350997f6ae1
yamakiller <296270991@qq.com>
2019-12-05 21:56:15 +0800

    append error message

---
+++ code/error.go
@@ -0,0 +1,18 @@
+package code
+
+import "errors"
+
+var (
+	//ErrIncompleteData error
+	ErrIncompleteData = errors.New("Incomplete data")
+	//ErrMethodName  error
+	ErrMethodName = errors.New("Protocol exception method name exceeded")
+	//ErrDataOverflow error
+	ErrDataOverflow = errors.New("Data overflow")
+	//ErrMethodUndefined error
+	ErrMethodUndefined = errors.New("RPC method undefined")
+	//ErrParamUndefined error
+	ErrParamUndefined = errors.New("RPC param undefined")
+	//ErrConnectFull error
+	ErrConnectFull = errors.New("Connection is full")
+)
