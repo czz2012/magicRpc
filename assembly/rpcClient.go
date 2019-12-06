ac886ad8352012dafaf2765b9798b49b673c7212
296270991@qq.com <296270991@qq.com>
2019-12-06 13:08:02 +0800

    append rpcTarget

--- assembly/rpcClient.go
+++ assembly/rpcClient.go
@@ -1,8 +1,26 @@
 package assembly
 
-import "github.com/yamakiller/magicNet/handler"
+import "github.com/yamakiller/magicNet/handler/implement"
+
+/*
+	GetName() string
+	GetAddr() string
+	GetOutSize() int
+	IsTimeout() uint64
+	RestTimeout()
+	SetStatus(stat NetConnectStatus)
+	GetStatus() NetConnectStatus
+*/
+
+type rpcTarget struct {
+	_addr string
+}
+
+func (slf *rpcTarget) GetAddr() string {
+	return slf._addr
+}
 
 //RPCClient doc
 type RPCClient struct {
-	handler.Service
+	implement.NetConnectService
 }
